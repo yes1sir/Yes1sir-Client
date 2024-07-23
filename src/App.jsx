bbe38437d200a62002a0af871387a24f25135bd2
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-07-24 02:00:52 +0900

    init: 글로벌 스타일 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <RouterProvider router={router} />
+      <p>안녕</p>
     </>
   );
 }
