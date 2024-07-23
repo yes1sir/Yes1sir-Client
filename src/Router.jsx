23be466295f7a329745ec2635307a9ad2ed29648
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-07-24 01:47:47 +0900

    init: 폴더구조 + 라우팅 설정

---
+++ src/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  //   {
+  //     path: "/1",
+  //     element: <Page1 />,
+  //   },
+  //   {
+  //     path: "/2",
+  //     element: <Page2 />,
+  //   },
+  //   {
+  //     path: "/3",
+  //     element: <Page3 />,
+  //   },
+]);
