e010ab652c6673c6f5ee37e95a9c2fb07c916962
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-08-01 18:24:23 +0900

    style: mbticontent 내부 버튼 생성

--- src/components/HomeLogin/HomeLoginMbtiContent.jsx
+++ src/components/HomeLogin/HomeLoginMbtiContent.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import HomeLoginBtn from "@/components/common/HomeLoginBtn";
 
 function HomeLoginMbtiContent({ $bgColor, content, title }) {
   return (
     <HomeLoginMbtiContentWrapper $bgColor={$bgColor}>
       <ContentTitle>{title}</ContentTitle>
       <ContentText>{content}</ContentText>
+      <HomeLoginBtnContainer>
+        <HomeLoginBtn
+          backgroundColor="#FFF"
+          color="black"
+          text="제품 추천 받기"
+        />
+      </HomeLoginBtnContainer>
     </HomeLoginMbtiContentWrapper>
   );
 }
@@ -21,8 +29,9 @@ export default HomeLoginMbtiContent;
 const HomeLoginMbtiContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   width: 144rem;
-  padding: 7.5rem 10rem;
+  padding: 7.5rem 10rem 5rem;
   gap: 3.75rem;
   background-color: ${(props) => props.$bgColor};
 `;
@@ -33,6 +42,11 @@ const ContentTitle = styled.p`
 `;
 
 const ContentText = styled.p`
+  text-align: start;
   color: ${({ theme }) => theme.colors.w01};
   ${({ theme }) => theme.fonts.M3_content_xsmall};
+`;
+
+const HomeLoginBtnContainer = styled.div`
+  margin-left: 100rem;
 `;
