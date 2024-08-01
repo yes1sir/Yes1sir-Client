aea2384f5c4f28c911cddee4c81465414cb8b940
hyun907 <seunghyun020907@naver.com>
2024-08-02 07:44:01 +0900

    style: 코드 스타일 및 뒤로가기 버튼 css 수정

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,11 +1,13 @@
 import { useState, useCallback, useEffect } from "react";
 import styled from "styled-components";
+
 import SelectBox from "../../components/Target/SelectBox";
 import HomeLoginBtn from "../../components/common/HomeLoginBtn";
 import ProgressIndicator from "../../components/Test/ProgressIndicator";
 
 import testList from "@/constants/Test/testList";
 import typeSum from "@/constants/Test/typeSum";
+import ArrowBackIcon from "@/assets/svgs/arrow_back.svg?react";
 
 function Test() {
   const [list, setList] = useState(typeSum);
@@ -14,32 +16,35 @@ function Test() {
   const [selectedAnswerIndices, setSelectedAnswerIndices] = useState({});
 
   // 선택된 답변 처리
-  const handleAnswer = useCallback((type, count, idx) => {
-    setSelectedAnswer({ type, count, idx });
+  const handleAnswer = useCallback(
+    (type, count, idx) => {
+      setSelectedAnswer({ type, count, idx });
 
-    setSelectedAnswerIndices(prev => ({
-      ...prev,
-      [page]: idx
-    }));
-  }, [page]);
+      setSelectedAnswerIndices((prev) => ({
+        ...prev,
+        [page]: idx,
+      }));
+    },
+    [page]
+  );
 
   // 뒤로가기 처리
   const handleGoBack = useCallback(() => {
     if (page > 0) {
       // 현재 페이지를 이전 페이지로 업데이트
       setPage((prevPage) => prevPage - 1);
-  
+
       // 이전 페이지의 답변 제거 및 점수 조정
       setList((prevList) => {
         const newList = [...prevList];
         const prevQuestion = testList[page - 1]; // 이전 페이지 질문
         const prevAnswerIndex = selectedAnswerIndices[page - 1]; // 이전 페이지에서 선택된 답변의 인덱스
-  
+
         if (prevQuestion) {
           // 선택된 답변의 유형과 점수
           const answerType = prevQuestion.a[prevAnswerIndex]?.type;
           const answerCount = prevQuestion.a[prevAnswerIndex]?.count;
-  
+
           // 점수 감소
           for (let i = 0; i < newList.length; i++) {
             if (answerType === newList[i].name) {
@@ -49,10 +54,10 @@ function Test() {
             }
           }
         }
-  
+
         return newList;
       });
-  
+
       // 선택된 답변 인덱스도 업데이트
       setSelectedAnswerIndices((prevIndices) => {
         const newIndices = { ...prevIndices };
@@ -61,14 +66,13 @@ function Test() {
       });
     }
   }, [page, selectedAnswerIndices]);
-  
 
   // 답변 선택 후 목록 업데이트 및 페이지 이동
   useEffect(() => {
     let timer;
     if (selectedAnswer) {
       timer = setTimeout(() => {
-        setList(prevList => {
+        setList((prevList) => {
           const newList = [...prevList];
           for (let i = 0; i < newList.length; i++) {
             if (selectedAnswer.type === newList[i].name) {
@@ -79,7 +83,7 @@ function Test() {
           return newList;
         });
 
-        setPage(prevPage => {
+        setPage((prevPage) => {
           const nextPage = prevPage + 1;
           if (nextPage >= testList.length) {
             setMbti(); // 모든 질문이 완료되면 MBTI 계산
@@ -110,13 +114,20 @@ function Test() {
   }
 
   useEffect(() => {
-    console.log('Current selectedAnswerIndices:', selectedAnswerIndices);
+    console.log("Current selectedAnswerIndices:", selectedAnswerIndices);
   }, [selectedAnswerIndices]);
 
   return (
     <TestWrapper>
+      {page >= 1 && (
+        <BackWrapper>
+          <BackBtn role="button" onClick={handleGoBack}>
+            <ArrowBackIcon />
+          </BackBtn>
+        </BackWrapper>
+      )}
+
       <ProgressIndicator page={page} lastPage={testList.length} />
-      <div onClick={handleGoBack}>뒤로가기</div>
       <div>
         {testList.map((val, idx) => (
           <div key={idx} style={{ display: page === idx ? "block" : "none" }}>
@@ -160,6 +171,17 @@ function Test() {
 }
 
 export default Test;
+
+const BackWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  padding-left: 50px;
+  padding-top: 23px;
+`;
+
+const BackBtn = styled.div`
+  cursor: pointer;
+`;
 
 const TestWrapper = styled.div`
   display: flex;
