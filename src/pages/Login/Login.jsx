84f964a23684e7c12c67346b4f04fafa7007e8d6
hyun907 <seunghyun020907@naver.com>
2024-08-03 21:44:58 +0900

    feat: 클라이언트 로그인 구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,44 @@
 import styled from "styled-components";
 import Backspace from "../../components/common/Backspace";
-import GoogleIcon from "@/assets/svgs/google_logo.svg?react";
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function Login() {
+  const clientId = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 처음 렌더링 시 로그인 상태 확인
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (isLoggedIn) {
+      navigate("/homelogin");
+    }
+  }, [navigate]);
+
+  const handleLoginSuccess = (credentialResponse) => {
+    const decodedToken = jwtDecode(credentialResponse.credential);
+    console.log(decodedToken);
+
+    // 로그인 성공 - 로컬 스토리지에 로그인 상태 저장
+    localStorage.setItem("isLoggedIn", "true");
+
+    // 새로운 사용자인지 확인 (로컬 스토리지에 없는지 확인)
+    const userId = localStorage.getItem("userId");
+    if (!userId) {
+      // 새로운 사용자라면 userId 저장 후 age 페이지로 이동
+      localStorage.setItem("userId", decodedToken.sub);
+      navigate("/age");
+    } else {
+      // 기존 사용자라면 homelogin 페이지로 이동
+      navigate("/homelogin");
+      // 그런데 이렇게 useEffect 사용해서 기존 사용자 / 새로운 사용자 구분해서 페이지 이동 강제로 시키면...
+      // homelogin 페이지는 지금까지의 로직 상 test 페이지에서 값을 받아오는데 test 페이지에서 피부 타입 결과를 서버에 저장시키고 불러와야 할 것 같다
+    }
+  };
+
   return (
     <LoginWrapper>
       <Backspace />
@@ -12,16 +48,22 @@ function Login() {
         <Testdescription>16가지 유형의 피부 MBTI 테스트</Testdescription>
       </LoginTop>
 
-      <LoginBottom>
-        <Logindescription>로그인</Logindescription>
-        <LoginBtn>
-          <GoogleIcon>icon</GoogleIcon>
-          <LoginBtnContent>Google로 로그인</LoginBtnContent>
-        </LoginBtn>
-      </LoginBottom>
+      <Logindescription>로그인</Logindescription>
+
+      <LoginBtn>
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
+      </LoginBtn>
     </LoginWrapper>
   );
 }
+
 const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
