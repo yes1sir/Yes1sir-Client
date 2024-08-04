37903015cb71ef356ad0d8403e196df58980c058
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-08-04 22:12:14 +0900

    feat: 로고 svg 반영 및 navigate 구현

--- src/components/common/TopNav.jsx
+++ src/components/common/TopNav.jsx
@@ -1,15 +1,22 @@
 import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom"; // useNavigate 훅 import
 import useNavigateToLogin from "@/utils/common/useNavigateToLogin";
 
 import HamburgerIcon from "@/assets/svgs/hamburger.svg?react";
+import LogoIcon from "@/assets/svgs/Logo.svg?react";
 
 function TopNav() {
+  const navigate = useNavigate();
   const navigateToLogin = useNavigateToLogin();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
 
   return (
     <HeaderWrapper>
-      <LogoBtn>LOGO</LogoBtn>
       <HeaderContainer>
+        <StyledLogoIcon onClick={handleLogoClick} />{" "}
         <LoginBtn onClick={navigateToLogin}>Log In</LoginBtn>
         <HamburgerIcon />
       </HeaderContainer>
@@ -27,20 +34,11 @@ const HeaderWrapper = styled.header`
   background-color: ${({ theme }) => theme.colors.b01};
 `;
 
-const LogoBtn = styled.button`
-  width: 8.3rem;
-  height: 3.1111rem;
-  margin-left: 4.9rem;
-  color: ${({ theme }) => theme.colors.w01};
-  text-align: center;
-  ${({ theme }) => theme.fonts.M3_headline_large};
-`;
-
 const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
-  gap: 4.2rem;
-  margin-left: 109.9rem;
+  padding: 0rem 2.6rem;
+  gap: 114.1rem;
 `;
 
 const LoginBtn = styled.button`
@@ -48,4 +46,8 @@ const LoginBtn = styled.button`
   height: 4rem;
   color: ${({ theme }) => theme.colors.w01};
   ${({ theme }) => theme.fonts.M3_headline_large};
+`;
+
+const StyledLogoIcon = styled(LogoIcon)`
+  cursor: pointer;
 `;
