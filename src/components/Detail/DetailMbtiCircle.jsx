fd42e82cfe6d892bb2e6c84b720b4222aeca1e5a
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-08-06 16:58:03 +0900

    feat: mbti circle 생성

---
+++ src/components/Detail/DetailMbtiCircle.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+
+function DetailMbtiCircle() {
+  const location = useLocation();
+  const { $bgColor } = location.state;
+
+  return (
+    <DetailMbtiCircleWrapper>
+      <Circle $bgColor={$bgColor}></Circle>
+    </DetailMbtiCircleWrapper>
+  );
+}
+
+export default DetailMbtiCircle;
+
+const DetailMbtiCircleWrapper = styled.div`
+  display: flex;
+`;
+
+const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 6.8rem;
+  height: 6.8rem;
+  border-radius: 50%;
+  background-color: ${({ $bgColor }) => $bgColor};
+`;
